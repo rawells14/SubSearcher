d72d54f3cde5a93fe612f91d1f1f2fe468ba9c51
Kurtis David <kurtis.david@live.com>
2016-10-01 20:05:38 -0500

    icon changes

--- background.js
+++ background.js
@@ -4,19 +4,47 @@
 
 function updateIcon(){
 
-	var current_url;
-	chrome.tabs.query({'active': true, 'lastFocusedWindow': true}, function (tabs) {
-	    current_url = tabs[0].url;
-	});
+	var url;
 
-	if (current_url.includes("youtube.com/watch?")){
+	var queryInfo = {
+	    active: true,
+	    currentWindow: true
+  	};
 
-		console.log("lol")
+	chrome.tabs.query(queryInfo, function(tabs) {
+		// chrome.tabs.query invokes the callback with a list of tabs that match the
+		// query. When the popup is opened, there is certainly a window and at least
+		// one tab, so we can safely assume that |tabs| is a non-empty array.
+		// A window can only have one active tab at a time, so the array consists of
+		// exactly one tab.
+		var tab = tabs[0];
+		// A tab is a plain object that provides information about the tab.
+		// See https://developer.chrome.com/extensions/tabs#type-Tab
+		url = tab.url;
 
-	}
+		// tab.url is only available if the "activeTab" permission is declared.
+		// If you want to see the URL of other tabs (e.g. after removing active:true
+		// from |queryInfo|), then the "tabs" permission is required to see their
+		// "url" properties.
+		console.assert(typeof url == 'string', 'tab.url should be a string');
 
+		if (url.includes("youtube.com/watch?")){
+			
+			chrome.browserAction.setIcon({path: "icon1.png"})
+
+		}
+		else{
+
+			chrome.browserAction.setIcon({path: "icon.png"})
+		}
+	  	});
 
 }
 
-window.onload = updateIcon();
+chrome.tabs.onUpdated.addListener(function(tabId , info) {
+    if (info.status == "complete") {
+        updateIcon();
+    }
+});
 
+chrome.tabs.onActivated.addListener(updateIcon);
